1ab21fb257e215e979ea9ec166dead90cfd41195
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-22 14:10:53 +0900

    refactor : drop findById method
    refactor : Drop findById method
    
    Drop useless method
    
    Reference by yonghwan

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/repository/MemberRepository.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/repository/MemberRepository.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+
     Member save(Member member);
-    Optional<Member> findById(Long id);
+
     Optional<Member> findByName(String name);
+
     List<Member> findAll();
 }
--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/repository/MemoryMemberRepository.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/repository/MemoryMemberRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class MemoryMemberRepository implements MemberRepository{
+public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
@@ -21,19 +21,18 @@ public class MemoryMemberRepository implements MemberRepository{
     }
 
     @Override
-    public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
-
-    @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().contains(name))
-                .findAny();
+            .filter(member -> member.getName().contains(name))
+            .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
