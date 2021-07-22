ed716069ed51bf21b097261271abf5d12e6415f0
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-22 14:14:23 +0900

    test: Testcase for MemoryMemberRepository
    test: Testcase for MemoryMemberRepository
    
    Test 3 methods: save, findByName, findAll. Use @AfterEach to clear
    repository for every test.
    
    Reference by yonghwan

--- Project_A/Business_Card_Program/src/test/java/businesscardprogram/BusinessCardProgramApplicationTests.java
+++ Project_A/Business_Card_Program/src/test/java/businesscardprogram/BusinessCardProgramApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BusinessCardProgramApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
