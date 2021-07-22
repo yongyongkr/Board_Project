888c249e02580cc3d6b611b58019a4e913e6c159
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-22 17:32:18 +0900

    feat: MemberService
    feat: MemberService
    
    Create 4 methods: create, validateDuplicateMember, findMember, findOne.
    
    To create card, check redundancy with validateDuplicateMember().
    If all values(name, number, company) try to create again, throw
    exceptions with message "이미 제작 진행 중입니다".
    
    Reference by yonghwan

---
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/service/MemberService.java
@@ -0,0 +1,40 @@
+package businesscardprogram.service;
+
+import businesscardprogram.domain.Member;
+import businesscardprogram.repository.MemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원 가입
+    public Long create(Member member) {
+        //같은 이름, 전화번호, 회사를 가진 중복 회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .filter(m -> m.getNumber().equals(member.getNumber()) && m.getCompany()
+                .equals(member.getCompany()))
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 제작 진행 중입니다");
+            });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
