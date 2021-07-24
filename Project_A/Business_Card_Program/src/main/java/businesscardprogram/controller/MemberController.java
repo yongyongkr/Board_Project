56408c20bafb0880d6c45dfa0450daa23b2d26cc
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-24 14:48:09 +0900

    feat: Create business card
    feat: Order business card
    
    Implement process of create business card. After clicking "명함 제작"
    button in homepage, createMemberForm.html comes into view.
    3 input forms require name, number and company. By clicking button
    "등록", its information send to MemberForm as parameter and
    MemberController send it to memberService to store it in repository.
    Then, redirect to the homepage by @PostMapping.
    
    Reference by yonghwan

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberController.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberController.java
@@ -1,8 +1,14 @@
 package businesscardprogram.controller;
 
+import businesscardprogram.domain.Member;
 import businesscardprogram.service.MemberService;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member(form.getName(), form.getNumber(), form.getCompany());
+
+        memberService.create(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
