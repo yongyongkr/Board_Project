5c7a40eee579778790d1b0754b795186c6595c92
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-29 04:30:22 +0900

    feat: Repository
    feat: Repository
    
    Make 7 methods in ArticleRepository interface. It needs more updates.
    Implement methods in JpaArticleRepository class but it needs more time.
    
    Reference by yonghwan
    Date : 21.07.29

---
+++ project.b/guest.book/src/main/java/project/b/guest/book/repository/ArticleRepository.java
@@ -0,0 +1,18 @@
+package project.b.guest.book.repository;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import java.util.Optional;
+import project.b.guest.book.article.Article;
+
+public interface ArticleRepository {
+
+    Article save(Article article);
+    List<Article> findAll();
+    void delete(Long id);
+    void plus(Long id);
+    void minus(Long id);
+    boolean existOrNot(Article article);
+//    Optional<Time> validateTime(Article article);
+}
---
+++ project.b/guest.book/src/main/java/project/b/guest/book/repository/JpaArticleRepository.java
@@ -0,0 +1,67 @@
+package project.b.guest.book.repository;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import project.b.guest.book.article.Article;
+
+public class JpaArticleRepository implements ArticleRepository {
+
+    private final EntityManager em;
+
+    public JpaArticleRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Article save(Article article) {
+        em.persist(article);
+        return article;
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return em.createQuery("select a from Article a", Article.class).getResultList();
+    }
+
+    @Override
+    public void delete(Long id) {
+        em.createQuery("delete from Article a where a.id = :id");
+    }
+
+    @Override
+    public void plus(Long id) {
+        em.createQuery("update Article set likes = likes + 1 where id = :id");
+    }
+
+    @Override
+    public void minus(Long id) {
+        em.createQuery("update Article set dislikes = dislikes - 1 where id = :id");
+    }
+
+    @Override
+    public boolean existOrNot(Article article) {
+        try {
+            em.createQuery("select a from Article a where a.name = :name")
+                .setParameter("name", article.getName());
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+//    @Override
+//    public Optional<Time> validateTime(Article article) {
+//        String name = article.getName();
+//        Timestamp past = (Timestamp) em.createQuery(
+//                "select max(a.time) from Article a where a.name = :name")
+//            .setParameter("name", name)
+//            .getSingleResult();
+//        return (Optional<Time>) em.createQuery(
+//            "select timestampdiff(minute, :past, current_timestamp) from Article a"
+//                + "where a.name = :name")
+//            .setParameter("past", past).setParameter("name", name).getSingleResult();
+//    }
+}
