16f52b114dc833a6e8e087c3efc5aa9d8c3f4711
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-07-29 04:35:35 +0900

    feat: Controller, main homepage
    feat: Controller, main homepage
    
    Create main homepage and its controller. Other functions are not
    working yet. Require more updates.
    
    Reference by yonghwan
    Date : 21.07.29

---
+++ project.b/guest.book/src/main/java/project/b/guest/book/controller/ArticleForm.java
@@ -0,0 +1,12 @@
+package project.b.guest.book.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArticleForm {
+
+    private String name;
+    private String content;
+}
---
+++ project.b/guest.book/src/main/java/project/b/guest/book/controller/HomeController.java
@@ -0,0 +1,36 @@
+package project.b.guest.book.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.b.guest.book.article.Article;
+import project.b.guest.book.service.GuestBookService;
+
+@Controller
+public class HomeController {
+
+    private final GuestBookService guestBookService;
+
+    @Autowired
+    public HomeController(GuestBookService guestBookService) {
+        this.guestBookService = guestBookService;
+    }
+
+    @GetMapping("/")
+    public String homepage() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String posting(ArticleForm form) {
+        Article article = new Article();
+        article.setName(form.getName());
+        article.setContent(form.getContent());
+
+        guestBookService.upload(article);
+
+        return "home";
+    }
+
+}
