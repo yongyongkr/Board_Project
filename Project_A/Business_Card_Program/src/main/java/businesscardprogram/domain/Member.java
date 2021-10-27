8d0352cebac6df01416695ce5211d0a08913eb85
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-10-28 01:20:27 +0900

    Member 엔티티 수정
    - Getter 사용해서 코드 간단하게 수정
    - private로 잘못 선언된 생성자 protected로 수정
    - static 선언 안해줬던 팩토리 메서드 수정

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/domain/Member.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/domain/Member.java
@@ -4,8 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,30 +19,14 @@ public class Member {
     private String number;
     private String company;
 
-    private Member(String name, String number, String company) {
+    protected Member(String name, String number, String company) {
         this.name = name;
         this.number = number;
         this.company = company;
     }
 
-    public Member createMember(String name, String number, String company) {
+    public static Member createMember(String name, String number, String company) {
         Member member = new Member(name, number, company);
         return member;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public String getCompany() {
-        return company;
     }
 }
