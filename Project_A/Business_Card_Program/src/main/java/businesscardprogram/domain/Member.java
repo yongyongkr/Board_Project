556ad6c773a7f564db83e8cbf8097dafdcaaf4d8
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-10-27 09:40:29 +0900

    Member 엔티티 리팩토링
    public으로 선언된 생성자를 private로 접근 제어하고 createMember라는 팩토리 메서드 생성
    setter 메서드 제거 (외부에서 수정 가능성 제거)

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/domain/Member.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/domain/Member.java
@@ -14,44 +14,30 @@ public class Member {
     private String number;
     private String company;
 
-    public Member() {
-    }
-
-    public Member(String name, String number, String company) {
+    private Member(String name, String number, String company) {
         this.name = name;
         this.number = number;
         this.company = company;
+    }
+
+    public Member createMember(String name, String number, String company) {
+        Member member = new Member(name, number, company);
+        return member;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getNumber() {
         return number;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
     public String getCompany() {
         return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
     }
 }
--- project.b/guest.book/src/main/java/SpringConfig.java
+++ project.b/guest.book/src/main/java/SpringConfig.java
@@ -1,31 +1,31 @@
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import project.b.guest.book.repository.ArticleRepository;
-import project.b.guest.book.repository.JpaArticleRepository;
-import project.b.guest.book.service.GuestBookService;
-
-@Configuration
-public class SpringConfig {
-
-    private final DataSource dataSource;
-    private final EntityManager em;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
-    }
-
-    @Bean
-    public GuestBookService guestBookService(){
-        return new GuestBookService(articleRepository());
-    }
-
-    @Bean
-    public ArticleRepository articleRepository(){
-        return new JpaArticleRepository(em);
-    }
-}
+//import javax.persistence.EntityManager;
+//import javax.sql.DataSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import project.b.guest.book.repository.ArticleRepository;
+//import project.b.guest.book.repository.JpaArticleRepository;
+//import project.b.guest.book.service.GuestBookService;
+//
+//@Configuration
+//public class SpringConfig {
+//
+////    private final DataSource dataSource;
+////    private final EntityManager em;
+//
+////    @Autowired
+////    public SpringConfig(DataSource dataSource, EntityManager em) {
+////        this.dataSource = dataSource;
+////        this.em = em;
+////    }
+//
+//    @Bean
+//    public GuestBookService guestBookService(){
+//        return new GuestBookService(articleRepository());
+//    }
+//
+//    @Bean
+//    public ArticleRepository articleRepository(){
+//        return new JpaArticleRepository(em);
+//    }
+//}
