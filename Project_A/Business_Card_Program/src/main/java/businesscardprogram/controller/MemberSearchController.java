aa32b69d862f57fe4b12d2e51f5ed9d4bd59934b
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-10-30 06:13:04 +0900

    이름으로 조회 기능
    - MemberSearchController 리팩토링 (lombok 사용, Service 메소드 수정으로 인한 변수 수정
    - search.html 에서 head 추가 (한글 깨짐 문제로 인한)
    - search.html 에서 memberList 변수명으로 수정

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberSearchController.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberSearchController.java
@@ -1,23 +1,20 @@
 package businesscardprogram.controller;
 
 import businesscardprogram.domain.Member;
+import businesscardprogram.dto.MemberForm;
 import businesscardprogram.service.MemberService;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberSearchController {
 
     private final MemberService memberService;
-
-    @Autowired
-    public MemberSearchController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @GetMapping("/members/find")
     public String search(Model model) {
@@ -28,8 +25,8 @@ public class MemberSearchController {
     @PostMapping("/members/find")
     public String searchList(MemberForm form, Model model) {
         String name = form.getName();
-        Member member = memberService.searchMembers(name).get();
-        model.addAttribute("member", member);
+        List<Member> memberList = memberService.findMembersByName(name);
+        model.addAttribute("memberList", memberList);
 
         return "members/memberList";
     }
