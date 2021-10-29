ce1127ae51e041f6cc0a861329cb904a54fcc1bb
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-10-30 06:11:33 +0900

    전체 조회 기능
    - MemberSearchController 리팩토링 (lombok 사용, Service 메소드 변경으로 인한 변수명 수정
    - memberList.html에서 List를 memberList라는 좀 더 확실한 변수명으로 받아오게 수정

--- Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberController.java
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/controller/MemberController.java
@@ -1,26 +1,20 @@
 package businesscardprogram.controller;
 
 import businesscardprogram.domain.Member;
+import businesscardprogram.dto.MemberForm;
 import businesscardprogram.service.MemberService;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @GetMapping("/members/new")
     public String createForm() {
@@ -29,17 +23,15 @@ public class MemberController {
 
     @PostMapping("/members/new")
     public String create(MemberForm form) {
-        Member member = new Member(form.getName(), form.getNumber(), form.getCompany());
-
+        Member member = Member.createMember(form.getName(), form.getNumber(), form.getCompany());
         memberService.create(member);
-
         return "redirect:/";
     }
 
     @GetMapping("/members")
     public String list(Model model) {
-        List<Member> member = memberService.findMembers();
-        model.addAttribute("member", member);
+        List<Member> memberList = memberService.findAllMembers();
+        model.addAttribute("memberList", memberList);
         return "members/memberList";
     }
 }
