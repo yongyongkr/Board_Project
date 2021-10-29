28d9f22b1d533277d56862555e0120ead7213301
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-10-30 06:09:55 +0900

    Dto package 분리
    MemberForm을 controller package에 넣어뒀는데 아무리 규모가 작아도
    책임과 성격에 따라 분리해주기 위해 dto package를 별도로 생성해주었다

---
+++ Project_A/Business_Card_Program/src/main/java/businesscardprogram/dto/MemberForm.java
@@ -0,0 +1,13 @@
+package businesscardprogram.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    private String name;
+    private String number;
+    private String company;
+}
