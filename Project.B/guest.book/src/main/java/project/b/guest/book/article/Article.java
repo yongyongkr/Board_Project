d5b981a390cfe106e5d4c59b1cc82e45cec1cf5e
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-01 12:27:39 +0900

    Article 엔티티 리팩토링
    - setter 제거
    - protected 제어자로 생성자 수정
    - 팩토리 메소드를 통해 객체 생성되도록 수정
    - 변수명 조금 더 명확하게 변경
    - username, updateTime 변수 추가
    - time, likes, dislikes 값들은 service 레이어에서 초기화 예정

--- Project.B/guest.book/src/main/java/project/b/guest/book/article/Article.java
+++ Project.B/guest.book/src/main/java/project/b/guest/book/article/Article.java
@@ -6,26 +6,37 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
-@ToString
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String context;
-    private Timestamp time = Timestamp.valueOf(LocalDateTime.now());
-    private int likes = 0;
-    private int dislikes = 0;
+    private String title;
+    private String username;
+    private String content;
+    private Timestamp uploadTime;
+    private Timestamp updateTime;
+    private int likes;
+    private int dislikes;
+
+    protected Article(String title, String username, String content) {
+        this.title = title;
+        this.username = username;
+        this.content = content;
+    }
+
+    public static Article createArticle(String title, String username, String content) {
+        Article article = new Article(title, username, content);
+        return article;
+    }
 }
