534a015f281dceac6190de25981662f21949124a
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-04 04:06:23 +0900

    ArticleRepository 리팩토링
    - findById는 다시 Optional 객체로 반환하는 것으로 수정
      - 다시 생각해보니 article id 값으로 조회하면 1개의 게시글만 조회될 것
    - delete 메소드는 다시 void 반환형으로 변경

--- Project.B/guest.book/src/main/java/project/b/guest/book/repository/ArticleRepository.java
+++ Project.B/guest.book/src/main/java/project/b/guest/book/repository/ArticleRepository.java
@@ -1,19 +1,20 @@
 package project.b.guest.book.repository;
 
 import java.util.List;
+import java.util.Optional;
 import project.b.guest.book.domain.Article;
 
 public interface ArticleRepository {
 
     Long save(Article article);
 
-    List<Article> findById(Long id);
+    Optional<Article> findById(Long id);
 
     List<Article> findByName(String name);
 
     List<Article> findAll();
 
-    Long delete(Long id);
+    void delete(Long id);
 
     void plus(Long id);
 
