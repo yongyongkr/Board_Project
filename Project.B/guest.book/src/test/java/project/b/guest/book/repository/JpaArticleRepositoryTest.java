0e04730e9ddfe0eb29277f7ce4d6f21f33977de8
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-04 04:37:38 +0900

    application.yml 생성
    - application.properties 대신 yml 사용

--- Project.B/guest.book/src/test/java/project/b/guest/book/repository/JpaArticleRepositoryTest.java
+++ Project.B/guest.book/src/test/java/project/b/guest/book/repository/JpaArticleRepositoryTest.java
@@ -1,43 +1,68 @@
-/*
 package project.b.guest.book.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import project.b.guest.book.article.Article;
+import org.springframework.test.context.junit4.SpringRunner;
+import project.b.guest.book.domain.Article;
 
 @Transactional
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class JpaArticleRepositoryTest {
 
-    private final EntityManager em;
-    JpaArticleRepositoryTest(EntityManager em) {
-        this.em = em;
-    }
-
-    JpaArticleRepository jpaArticleRepository;
-
+    @Autowired
+    ArticleRepository jpaArticleRepository;
 
     @Test
     void save() {
         //given
-        Article article = new Article();
-        article.setName("철수");
-        article.setContext("hello");
+        Article article = Article.createArticle("후라이 만드는법", "철수", "후라이팬에 기름을 두르고 굽는다");
 
         //when
         jpaArticleRepository.save(article);
 
+        //then
+        assertThat(jpaArticleRepository.findAll().size()).isEqualTo(1);
     }
 
     @Test
     void findById() {
+        //given
+        Article article = Article.createArticle("후라이 만드는법", "철수", "후라이팬에 기름을 두르고 굽는다");
+
+        //when
+        jpaArticleRepository.save(article);
+        Article findArticle = jpaArticleRepository.findById(article.getId()).get();
+
+        //then
+        assertThat(findArticle.getId()).isEqualTo(article.getId());
+        assertThat(findArticle.getContent()).isEqualTo(article.getContent());
     }
 
     @Test
     void findByName() {
+        //given
+        Article article1 = Article.createArticle("후라이 만드는법", "철수", "후라이팬에 기름을 두르고 굽는다");
+        Article article2 = Article.createArticle("일기", "영희", "오늘 날씨 맑음");
+        Article article3 = Article.createArticle("밥 먹을 사람?", "철수", "10분 뒤에 우리집으로");
+
+        //when
+        jpaArticleRepository.save(article1);
+        jpaArticleRepository.save(article2);
+        jpaArticleRepository.save(article3);
+
+        //then
+        assertThat(jpaArticleRepository.findByName("철수").size()).isEqualTo(2);
+        assertThat(jpaArticleRepository.findByName("영희").size()).isEqualTo(1);
     }
 
     @Test
@@ -65,4 +90,4 @@ class JpaArticleRepositoryTest {
     @Test
     void validateTime() {
     }
-}*/
+}
