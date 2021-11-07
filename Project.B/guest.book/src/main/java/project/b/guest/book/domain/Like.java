c902e8ea7428de91c5c69db9e0e23c3619f67b4b
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-07 17:22:11 +0900

    Like 엔티티 생성
    - article과 comment와 각각 연관관계 맺을 예정

---
+++ Project.B/guest.book/src/main/java/project/b/guest/book/domain/Like.java
@@ -0,0 +1,31 @@
+package project.b.guest.book.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long likes;
+    private Long dislikes;
+
+    protected Like(Long likes, Long dislikes) {
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+
+    public static Like createLike() {
+        Like like = new Like(0L, 0L);
+        return like;
+    }
+}
