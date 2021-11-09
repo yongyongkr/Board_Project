533b6694b3452873c8e37848df7460a2ab8709d7
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-09 11:43:17 +0900

    CommentRepository 생성
    - 저장
    - commentId로 찾기
    - articleId로 찾기
    - 전체 조회
    - 삭제

---
+++ Project_B/Board_Program/src/main/java/boardprogram/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package boardprogram.repository;
+
+import boardprogram.domain.Comment;
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository {
+
+    Long save(Comment comment);
+
+    Optional<Comment> findById(Long id);
+
+    List<Comment> findByArticle(Long articleId);
+
+    List<Comment> findAll();
+
+    void delete(Long id);
+}
