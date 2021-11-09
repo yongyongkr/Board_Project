218216d737840b21a2e152022cd87d77cb4fa451
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-09 11:03:42 +0900

    Comment 엔티티 수정
    - 좋아요 기능 추가
      - 생성자에서 0으로 초기화
    - content는 @Lob 추가
    - 생성 시간, 수정 시간 추가

--- Project_B/Board_Program/src/main/java/boardprogram/domain/Comment.java
+++ Project_B/Board_Program/src/main/java/boardprogram/domain/Comment.java
@@ -1,9 +1,11 @@
 package boardprogram.domain;
 
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,11 +19,19 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
+    @Lob
     private String content;
+
     private boolean deleted;
+    private Integer likes;
+    private Integer dislikes;
+    private LocalDateTime createTime;
+    private LocalDateTime lastModifiedTime;
 
     protected Comment(String content) {
         this.content = content;
+        this.likes = 0;
+        this.dislikes = 0;
     }
 
     public static Comment createComment(String content) {
