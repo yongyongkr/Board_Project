5e0669085dcce407731aacc96134c1d5240e2a33
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-09 11:03:06 +0900

    Article 엔티티 수정
    - 좋아요 기능 추가
      - 생성자에서 0으로 초기화
    - content는 @Lob 추가

--- Project_B/Board_Program/src/main/java/boardprogram/domain/Article.java
+++ Project_B/Board_Program/src/main/java/boardprogram/domain/Article.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +21,12 @@ public class Article {
 
     private String title;
     private String username;
+
+    @Lob
     private String content;
+
+    private Integer likes;
+    private Integer dislikes;
     private LocalDateTime createTime;
     private LocalDateTime lastModifiedTime;
 
@@ -28,6 +34,8 @@ public class Article {
         this.title = title;
         this.username = username;
         this.content = content;
+        this.likes = 0;
+        this.dislikes = 0;
     }
 
     public static Article createArticle(String title, String username, String content) {
