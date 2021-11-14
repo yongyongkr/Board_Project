2aba8cc5945fcde34a8f5eb1a0b2d13eec54d490
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-14 11:21:02 +0900

    ArticleService 수정
    - articleRepository DI 제대로 해주기
    - upload 시 LastModifiedTime도 같이 세팅
    - 모든 게시글 조회하는 메소드 명 명확하게 변경
      - articleList() 에서 findAllArticles() 로
    - findArticleById 메소드 추가 : 반환형 Article 객체
    - findArticlesByUsername 메소드 추가 : List 반환
    - dislikes() 메소드 자체에 10개 이상되면 자동 삭제되는 기능 추가

--- Project_B/Board_Program/src/main/java/boardprogram/service/ArticleService.java
+++ Project_B/Board_Program/src/main/java/boardprogram/service/ArticleService.java
@@ -4,17 +4,21 @@ import boardprogram.domain.Article;
 import boardprogram.repository.ArticleRepository;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class ArticleService {
 
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
     public Long upload(Article article) {
         article.setCreateTime(LocalDateTime.now());
+        article.setLastModifiedTime(LocalDateTime.now());
         articleRepository.save(article);
         return article.getId();
     }
@@ -24,21 +28,27 @@ public class ArticleService {
         articleRepository.save(article);
     }
 
-    public List<Article> articleList() {
+    public List<Article> findAllArticles() {
         return articleRepository.findAll();
     }
 
-    public void deleteByDislikes(Article article) {
-        if (article.getDislikes() >= 10) {
-            articleRepository.delete(article.getId());
+    public Article findArticleById(Long articleId) throws Exception {
+        return articleRepository.findById(articleId).orElseThrow(
+            () -> new Exception("cannot find"));
+    }
+
+    public List<Article> findArticlesByUsername(String username) {
+        return articleRepository.findByName(username);
+    }
+
+    public void likes(Long articleId) {
+        articleRepository.plus(articleId);
+    }
+
+    public void dislikes(Long articleId) {
+        Integer dislike = articleRepository.minus(articleId);
+        if (dislike >= 10) {
+            articleRepository.delete(articleId);
         }
-    }
-
-    public void likes(Long id) {
-        articleRepository.plus(id);
-    }
-
-    public void dislikes(Long id) {
-        articleRepository.minus(id);
     }
 }
