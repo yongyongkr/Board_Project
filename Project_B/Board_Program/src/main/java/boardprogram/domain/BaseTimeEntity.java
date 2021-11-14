c9e5ed13067cdd4a8aea995151bb5f5d389a97bb
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-14 10:48:18 +0900

    BaseTimeEntity Setter 생성
    - Setter 만들어도 큰 문제 없을 듯

--- Project_B/Board_Program/src/main/java/boardprogram/domain/BaseTimeEntity.java
+++ Project_B/Board_Program/src/main/java/boardprogram/domain/BaseTimeEntity.java
@@ -3,8 +3,10 @@ package boardprogram.domain;
 import java.time.LocalDateTime;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @MappedSuperclass
 public class BaseTimeEntity {
 
