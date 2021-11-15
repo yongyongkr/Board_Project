2257abbe9909cdea92e3e957642326e4d577c65a
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-15 09:26:08 +0900

    ArticleController 생성
    - 메인 화면
      - 게시글 목록 순서대로 정렬
    - Article 화면
      - 제목, 작성일자, 작성자, 내용
    - SearchUsername 화면
      - 해당 작성자가 작성한 게시글 모두 조회

---
+++ Project_B/Board_Program/src/main/java/boardprogram/controller/ArticleController.java
@@ -0,0 +1,55 @@
+package boardprogram.controller;
+
+import boardprogram.domain.Article;
+import boardprogram.service.ArticleService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @GetMapping
+    public String Main(Model model) {
+        List<Article> articles = articleService.findAllArticles();
+        model.addAttribute("articles", articles);
+        return "main";
+    }
+
+    @GetMapping("/article/{articleId}")
+    public String Article(@PathVariable Long articleId, Model model) {
+        Article article = null;
+        try {
+            article = articleService.findArticleById(articleId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("article", article);
+        return "article/articlePage";
+    }
+
+    @GetMapping("/{username}/information")
+    public String SearchByUsername(@PathVariable String username, Model model) {
+        List<Article> articles = articleService.findArticlesByUsername(username);
+        model.addAttribute("articles", articles);
+        return "/article/SearchUsername";
+    }
+
+    @PostConstruct
+    public void init() {
+        Article article1 = Article.createArticle("후라이 만드는법", "철수", "후라이팬에 기름을 두르고 굽는다");
+        Article article2 = Article.createArticle("일기", "영희", "오늘 날씨 맑음");
+        Article article3 = Article.createArticle("밥 먹을 사람?", "철수", "10분 뒤에 우리집으로");
+
+        articleService.upload(article1);
+        articleService.upload(article2);
+        articleService.upload(article3);
+    }
+}
