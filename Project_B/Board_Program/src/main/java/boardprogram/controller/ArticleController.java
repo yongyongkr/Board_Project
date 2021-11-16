00952279f8932634d7ef98ba40a32a0605d07018
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-16 20:49:33 +0900

    ArticleController 기능 추가
    - Article 페이지에 해당 게시글의 댓글 목록 추가
    - update 관련 Get, Patch 메소드 추가
    - 삭제 관련 delete 메소드 추가

--- Project_B/Board_Program/src/main/java/boardprogram/controller/ArticleController.java
+++ Project_B/Board_Program/src/main/java/boardprogram/controller/ArticleController.java
@@ -2,14 +2,18 @@ package boardprogram.controller;
 
 import boardprogram.DTO.ArticleDTO;
 import boardprogram.domain.Article;
+import boardprogram.domain.Comment;
 import boardprogram.service.ArticleService;
+import boardprogram.service.CommentService;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final CommentService commentService;
 
     @GetMapping
     public String Main(Model model) {
@@ -34,7 +39,12 @@ public class ArticleController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        List<Comment> comments = commentService.findCommentsByArticleId(articleId);
+
         model.addAttribute("article", article);
+        model.addAttribute("comments", comments);
+
         return "article/articlePage";
     }
 
@@ -47,7 +57,7 @@ public class ArticleController {
 
     @GetMapping("/article/post")
     public String ArticleForm() {
-        return "/article/articleForm";
+        return "/article/articleCreateForm";
     }
 
     @PostMapping("/article/post")
@@ -55,6 +65,32 @@ public class ArticleController {
         Article article = Article.createArticle(articleDTO.getTitle(), articleDTO.getUsername(),
             articleDTO.getContent());
         articleService.upload(article);
+        return "redirect:/";
+    }
+
+    @GetMapping("/article/update/{articleId}")
+    public String UpdateForm(@PathVariable Long articleId) {
+        return "/article/articleUpdateForm";
+    }
+
+    @PatchMapping("/article/update/{articleId}")
+    public String UpdateArticle(@PathVariable Long articleId, ArticleDTO articleDTO) {
+        Article article = null;
+        try {
+            article = articleService.findArticleById(articleId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Article updatedArticle = Article.updateArticle(article, articleDTO.getTitle(),
+            articleDTO.getContent());
+
+        articleService.updateContent(article);
+        return "redirect:/article/" + articleId;
+    }
+
+    @DeleteMapping("/article/delete/{articleId}")
+    public String DeleteArticle(@PathVariable Long articleId) {
+        articleService.delete(articleId);
         return "redirect:/";
     }
 
