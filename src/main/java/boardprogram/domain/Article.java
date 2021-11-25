b3431f23ca58b936de9754ac1a429c5da19b757b
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-25 20:18:30 +0900

    feat<Article>: 멤버 엔티티와의 연관관계
    - 팩토리 메소드에 연관관계 설정

--- src/main/java/boardprogram/domain/Article.java
+++ src/main/java/boardprogram/domain/Article.java
@@ -19,57 +19,59 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "article_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_id")
+	private Long id;
 
-    private String title;
-    private String username;
+	private String title;
+	private String username;
 
-    @Lob
-    private String content;
+	@Lob
+	private String content;
 
-    private Integer view;
-    private Integer likes;
-    private Integer dislikes;
+	private Integer view;
+	private Integer likes;
+	private Integer dislikes;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-    protected Article(String title, String username, String content) {
-        this.title = title;
-        this.username = username;
-        this.content = content;
-        this.view = 1;
-        this.likes = 0;
-        this.dislikes = 0;
-    }
+	protected Article(String title, String username, String content) {
+		this.title = title;
+		this.username = username;
+		this.content = content;
+		this.view = 1;
+		this.likes = 0;
+		this.dislikes = 0;
+	}
 
-    public static Article createArticle(String title, String username, String content) {
-        return new Article(title, username, content);
-    }
+	public static Article createArticle(Member member, String title, String username, String content) {
+		Article article = new Article(title, username, content);
+		article.setMemberRelation(member);
+		return article;
+	}
 
-    public static Article updateArticle(Article article, String title, String content) {
-        article.setTitle(title);
-        article.setContent(content);
-        return article;
-    }
+	public static Article updateArticle(Article article, String title, String content) {
+		article.setTitle(title);
+		article.setContent(content);
+		return article;
+	}
 
-    public void setMemberRelation(Member member) {
-        this.member = member;
-    }
+	public void setMemberRelation(Member member) {
+		this.member = member;
+	}
 
-    private void setTitle(String title) {
-        this.title = title;
-    }
+	private void setTitle(String title) {
+		this.title = title;
+	}
 
-    private void setContent(String content) {
-        this.content = content;
-    }
+	private void setContent(String content) {
+		this.content = content;
+	}
 
-    public void addView() {
-        this.view = this.view + 1;
-    }
+	public void addView() {
+		this.view = this.view + 1;
+	}
 }
