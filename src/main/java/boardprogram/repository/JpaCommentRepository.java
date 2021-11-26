46529475cd6f3e4eb58b695ec3e5aefdca3b5730
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-26 22:59:25 +0900

    style: 캠퍼스 핵데이 java 코딩 컨벤션 사용
    기존의 구글 스타일 가이브에서 캠퍼스 핵데이 자바 코딩 컨벤션으로 변경

--- src/main/java/boardprogram/repository/JpaCommentRepository.java
+++ src/main/java/boardprogram/repository/JpaCommentRepository.java
@@ -1,79 +1,82 @@
 package boardprogram.repository;
 
-import boardprogram.domain.Comment;
 import java.util.List;
 import java.util.Optional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
+
+import boardprogram.domain.Comment;
 
 @Repository
 public class JpaCommentRepository implements CommentRepository {
 
-    @PersistenceContext
-    EntityManager em;
+	@PersistenceContext
+	EntityManager em;
+
+	@Override
+	public Long save(Comment comment) {
+		em.persist(comment);
+		return comment.getId();
+	}
+
+	@Override
+	public Optional<Comment> findById(Long id) {
+		return Optional.ofNullable(em.find(Comment.class, id));
+	}
+
+	@Override
+	public List<Comment> findByArticle(Long articleId) {
+		return em.createQuery(
+				"select c from Comment c where c.article.id = :id order by c.createTime desc",
+				Comment.class)
+			.setParameter("id", articleId)
+			.getResultList();
+	}
 
     @Override
-    public Long save(Comment comment) {
-        em.persist(comment);
-        return comment.getId();
-    }
+	public List<Comment> findAll() {
+		return em.createQuery("select c from Comment c", Comment.class).getResultList();
+	}
 
-    @Override
-    public Optional<Comment> findById(Long id) {
-        return Optional.ofNullable(em.find(Comment.class, id));
-    }
+	@Override
+	public List<Comment> orderByLikes() {
+		return em.createQuery("select c from Comment c order by c.likes desc", Comment.class)
+			.getResultList();
+	}
 
-    @Override
-    public List<Comment> findByArticle(Long articleId) {
-        return em.createQuery(
-                "select c from Comment c where c.article.id = :id order by c.createTime desc",
-                Comment.class)
-            .setParameter("id", articleId)
-            .getResultList();
-    }
+	@Override
+	public Long delete(Long commentId) {
+		em.createQuery("update Comment c set c.content = :content where c.id = :id")
+			.setParameter("content", "삭제된 댓글입니다")
+			.setParameter("id", commentId)
+			.executeUpdate();
+		em.clear();
 
-    @Override
-    public List<Comment> findAll() {
-        return em.createQuery("select c from Comment c", Comment.class).getResultList();
-    }
+		return commentId;
+	}
 
-    @Override
-    public List<Comment> orderByLikes() {
-        return em.createQuery("select c from Comment c order by c.likes desc", Comment.class)
-            .getResultList();
-    }
+	@Override
+	public Integer plus(Long commentId) {
+		em.createQuery("update Comment c set c.likes = c.likes + 1 where c.id = :id")
+			.setParameter("id", commentId)
+			.executeUpdate();
+		em.clear();
 
-    @Override
-    public Long delete(Long commentId) {
-        em.createQuery("update Comment c set c.content = :content where c.id = :id")
-            .setParameter("content", "삭제된 댓글입니다")
-            .setParameter("id", commentId)
-            .executeUpdate();
-        em.clear();
+		Comment findComment = em.find(Comment.class, commentId);
+		return findComment.getLikes();
+	}
 
-        return commentId;
-    }
+	@Override
+	public Integer minus(Long commentId) {
+		em.createQuery("update Comment c set c.dislikes = c.dislikes + 1 where c.id = :id")
+			.setParameter("id", commentId)
+			.executeUpdate();
+		em.clear();
 
-    @Override
-    public Integer plus(Long commentId) {
-        em.createQuery("update Comment c set c.likes = c.likes + 1 where c.id = :id")
-            .setParameter("id", commentId)
-            .executeUpdate();
-        em.clear();
-
-        Comment findComment = em.find(Comment.class, commentId);
-        return findComment.getLikes();
-    }
-
-    @Override
-    public Integer minus(Long commentId) {
-        em.createQuery("update Comment c set c.dislikes = c.dislikes + 1 where c.id = :id")
-            .setParameter("id", commentId)
-            .executeUpdate();
-        em.clear();
-
-        Comment findComment = em.find(Comment.class, commentId);
-        return findComment.getDislikes();
-    }
+		Comment findComment = em.find(Comment.class, commentId);
+		return findComment.getDislikes();
+	}
 }
